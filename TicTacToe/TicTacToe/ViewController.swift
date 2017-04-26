d337e1c904105056f0f99fbe4340e52a37440462
Sara <saraahmad1114@yahoo.com>
2017-04-25 23:34:42 -0400

    board complete

--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -27,13 +27,19 @@ class ViewController: UIViewController {
 
   
     @IBAction func actionButton(_ sender: UIButton) {
-        if self.firstPlayer == 1 {
-            sender.setImage(UIImage(named: "Cross.png"), for: UIControlState())
-            firstPlayer = 2
-        }
-        else{
+        
+        if self.gamePlace[sender.tag-1] == 0{
+            
+            self.gamePlace[sender.tag-1] = self.firstPlayer
+        
+            if self.firstPlayer == 1 {
+                sender.setImage(UIImage(named: "Cross.png"), for: UIControlState())
+                firstPlayer = 2
+            }
+            else{
             sender.setImage(UIImage(named: "Nought.png"), for: UIControlState())
             firstPlayer = 1
+            }
         }
     }
    
